d6a960d9aba38c3ede7842b140a9a0a728565880
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2026-01-10 04:37:55 +0300

    13

--- database/migrations/2026_01_10_012719_create_links_table.php
+++ database/migrations/2026_01_10_012719_create_links_table.php
@@ -6,20 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('links', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary()->unique();
+            $table->string('title');
+            $table->longText('subtitle')->nullable();
+            $table->string('slug');
+            $table->string('url')->nullable();
+            $table->uuid('linkable_id')->nullable();
+            $table->string('linkable_type')->nullable();
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->index('title', 'title_index');
+            $table->index('url', 'url_index');
+            $table->index(['linkable_id', 'linkable_type'], 'linkable_index');
+            $table->fullText(['title', 'url'], 'fulltext_title_url');
+            $table->unique(['title', 'linkable_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('links');
