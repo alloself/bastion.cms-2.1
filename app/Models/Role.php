52c5653ff77068873c1badc8e985a5838d4ec6ca
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2026-01-11 00:16:50 +0300

    123

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,7 +7,7 @@ use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
-    use HasFactory;
-    use HasUuids;
+    use HasFactory, HasUuids;
+    
     protected $primaryKey = 'uuid';
 }
