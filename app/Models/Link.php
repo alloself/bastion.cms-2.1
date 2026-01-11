90e7902e7877a7bb678401e27ee352e96d3814aa
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2026-01-11 12:27:11 +0300

    123

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -3,21 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Link extends CRUDModel
 {
+    use HasSlug;
+
     protected $fillable = [
         'title',
         'subtitle',
+        'slug',
         'url',
         'linkable_id',
         'linkable_type',
     ];
 
-    protected array $searchable = ['title', 'url'];
+    protected array $searchable = ['title', 'url', 'slug'];
 
     public function linkable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function getSlugOptions()
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->allowDuplicateSlugs();
     }
 }
