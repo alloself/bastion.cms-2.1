761e725919fede71b349587dfd04a487305c78d1
Алексей Цапаев <aleksei.tsapaev@yasno.live>
2026-03-08 22:26:29 +0300

    435345

--- resources/admin/ts/app/plugins/index.ts
+++ resources/admin/ts/app/plugins/index.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import router from '../router'
 import vuetify from './vuetify'
 
-export function registerPlugins(app: App) {
+export const registerPlugins = (app: App) => {
   app.use(vuetify)
   app.use(createPinia())
   app.use(router)
